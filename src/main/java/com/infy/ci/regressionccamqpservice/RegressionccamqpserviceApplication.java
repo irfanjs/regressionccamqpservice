778dabcafc61e386322784b8fa5c50f33ad22476
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-20 19:08:28 +0530

    code modify

---
+++ src/main/java/com/infy/ci/regressionccamqpservice/RegressionccamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.regressionccamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegressionccamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegressionccamqpserviceApplication.class, args);
+	}
+
+}
